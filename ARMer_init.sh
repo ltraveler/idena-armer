86f63712979e04596b31962b5e5122ddebeaeb13
LTraveler <ltraveler@protonmail.com>
2022-09-27 05:38:34 +0000

    idena update script has been added

--- ARMer_init.sh
+++ ARMer_init.sh
@@ -63,7 +63,34 @@ fi
 
 mkdir /home/$username/idena-go
 cd /home/$username/idena-go
-wget -O idena-go https://github.com/ltraveler/idena-go-arm64/raw/main/idena-go
+
+read -p "Would you like me to compile idena-go bin file from the source code? " -n 1 -r
+echo  "Let's do some updates" 
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+        if ! command -v go &> /dev/null
+        then
+                echo "Compiling idena-go..."
+                wget -O go1.18.6.linux-arm64.tar.gz https://go.dev/dl/go1.18.6.linux-arm64.tar.gz
+                tar -xvf go1.18.6.linux-arm64.tar.gz
+                rm go1.18.6.linux-arm64.tar.gz
+                fi
+
+        GOROOT=/home/$username/idena-go/go
+        GOPATH=/home/$username/idena-go/go
+        PATH=$GOPATH/bin:$PATH
+
+        gover=$(curl -s https://api.github.com/repos/idena-network/idena-go/releases/latest | grep -Po '"tag_name":.*?[^\\]",' | sed 's/"tag_name": "v//g' |  sed 's/",//g')
+        wget -O v$gover.tar.gz https://github.com/idena-network/idena-go/archive/refs/tags/v$gover.tar.gz
+        tar -xzf v$gover.tar.gz && rm v$gover.tar.gz
+        cd idena-go-$gover
+
+        go get github.com/lucas-clemente/quic-go@v0.26.0
+        go build -ldflags "-X main.version=$gover" && chmod +x idena-go && cp -r idena-go /home/$username/idena-go/ && cd /home/$username/idena-go && rm -rf ./idena-go-$gover ./go
+else
+        wget -O idena-go https://github.com/ltraveler/idena-go-arm64/raw/main/idena-go
+fi
+
 chmod +x idena-go
 mkdir -p /home/$username/idena-go/datadir/idenachain.db
 cd /home/$username/idena-go/datadir/idenachain.db
---
+++ armer_update.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/bash
+ARMER_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+username=$(echo "$ARMER_DIR" | awk -F\/ '{print $3}')
+if [ -d "/home/$username/idena-go" ] 
+then
+	cd /home/$username/idena-go/
+	service idena stop
+else
+	echo "Error: IDENA ARMer is not installed."
+	exit
+fi
+
+cd /home/$username/idena-go/
+read -p "Would you like me to compile idena-go bin file from the source code?" -n 1 -r
+echo "Let's do some updates..." 
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+        if ! command -v go &> /dev/null
+        then
+                echo "Compiling idena-go..."
+                wget -O go1.18.6.linux-arm64.tar.gz https://go.dev/dl/go1.18.6.linux-arm64.tar.gz
+                tar -xvf go1.18.6.linux-arm64.tar.gz
+                rm go1.18.6.linux-arm64.tar.gz
+                fi
+
+        GOROOT=/home/$username/idena-go/go
+        GOPATH=/home/$username/idena-go/go
+        PATH=$GOPATH/bin:$PATH
+
+        gover=$(curl -s https://api.github.com/repos/idena-network/idena-go/releases/latest | grep -Po '"tag_name":.*?[^\\]",' | sed 's/"tag_name": "v//g' |  sed 's/",//g')
+        wget -O v$gover.tar.gz https://github.com/idena-network/idena-go/archive/refs/tags/v$gover.tar.gz
+        tar -xzf v$gover.tar.gz && rm v$gover.tar.gz
+        cd idena-go-$gover
+
+        go get github.com/lucas-clemente/quic-go@v0.26.0
+        go build -ldflags "-X main.version=$gover" && chmod +x idena-go && cp -r idena-go /home/$username/idena-go/ && cd /home/$username/idena-go && rm -rf ./idena-go-$gover ./go
+	service idena start
+else
+        wget -O idena-go https://github.com/ltraveler/idena-go-arm64/raw/main/idena-go && chmod +x idena-go
+	service idena start
+fi
+
